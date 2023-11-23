366facea5bbbb587a253f95d98b214733df614db
jameelag1995 <agbariajameel@gmail.com>
2023-11-23 11:08:16 +0200

    react buttons

---
+++ react-buttons/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from './components/Button'
+function App() {
+  
+
+  return (
+    <>
+      <Button text='Important'/>
+      <Button text='Not Important'/>
+    </>
+  )
+}
+
+export default App
