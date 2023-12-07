65b213a42baae0add2e41fbe69e5562a73f8fbb1
jameel Agbaria <agbariajameel@gmail.com>
2023-12-06 18:52:18 -0800

    usestate and useref problem with focus

---
+++ word-spelling-game/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import GuessTheWordGame from "./components/GuessTheWordGame/GuessTheWordGame.jsx";
+function App() {
+    return (
+        <div className="App">
+            <GuessTheWordGame />
+        </div>
+    );
+}
+
+export default App;
---
+++ word-spelling-game/src/components/GuessTheWordGame/GuessTheWordGame.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useRef, useState } from "react";
+import { wordsArray } from "../../constants/data";
+import "./GuessTheWordGame.css";
+
+function generateRandomWord() {
+    let word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    return word.toLowerCase();
+}
+
+const GuessTheWordGame = () => {
+    const [wordToGuess, setWordToGuess] = useState(generateRandomWord());
+    const [inputsValues, setInputsValues] = useState(
+        Array(wordToGuess.length).fill("")
+    );
+    const inputsRef = useRef([]);
+    const [isCorrect, setIsCorrect] = useState(
+        Array(wordToGuess.length).fill(false)
+    );
+    console.log("the word to guess is:", wordToGuess);
+    const handleInputChange = (i, value) => {
+        const newInputValues = [...inputsValues];
+        newInputValues[i] = value.toLowerCase();
+        console.log(value);
+        console.log("letter to guess", wordToGuess[i]);
+        setInputsValues(newInputValues);
+
+        if (value.toLowerCase() === wordToGuess[i].toLowerCase()) {
+            isCorrect[i] = true;
+        }
+        if (i !== inputsValues.length - 1) {
+            console.log("inside i < inpvals");
+            inputsRef.current[i + 1].focus();
+        }
+    };
+
+    console.log("after set", inputsValues);
+    return (
+        <div className="GuessTheWordGame">
+            <h1>Guess The Word Game</h1>
+            <div className="letters-inputs-container">
+                {inputsValues.map((letter, index) => {
+                    return (
+                        <input
+                            className={
+                                letter === wordToGuess[index] ? "correct" : ""
+                            }
+                            key={Math.random()}
+                            type="text"
+                            maxLength={1}
+                            ref={(el) => (inputsRef.current[index] = el)}
+                            onChange={(e) =>
+                                handleInputChange(index, e.target.value)
+                            }
+                            value={letter.toLowerCase()}
+                        />
+                    );
+                })}
+            </div>
+            {inputsValues.join("") === wordToGuess && (
+                <div className="won-container">
+                    <h2>You Won The Word Was {wordToGuess}</h2>
+                    <button onClick={()=>window.location.reload()}>Play Again</button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default GuessTheWordGame;
