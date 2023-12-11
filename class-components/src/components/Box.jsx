5b5842321fc6c3705074337ca45dc24bcc34d31f
jameel Agbaria <agbariajameel@gmail.com>
2023-12-11 22:21:40 +0200

    box animation class components

--- class-components/src/components/Box.jsx
+++ class-components/src/components/Box.jsx
@@ -8,33 +8,38 @@ class Box extends React.Component {
             boxNum: 0,
         };
     }
- 
-    shouldComponentUpdate() {
-        return true;
-    }
-
-    ComponentDidUpdate(prevProps, prevState) {
-        console.log(prevState);
-        if (prevState.boxNum != this.state.boxNum) {
-            const intervalid = setInterval(() => {
-                if (this.state.boxNum < 2) {
-                    this.setState((prevState) => {
-                        return { boxNum: prevState.boxNum + 1 };
-                    });
-                }
-            }, 5000);
-            return () => {
-                clearInterval(intervalid);
-            };
-        }
-    }
-    render() {
+    componentDidMount() {
+        // Set up an interval to increase the state every 5 seconds
+        this.intervalId = setInterval(() => {
+          this.setState((prevState) => {
+            const newNumber = prevState.boxNum + 1;
+    
+            if (newNumber <= 3) {
+              return {
+                boxNum: newNumber,
+              };
+            } else {
+              // Clear the interval if yourNumber reaches 3
+              clearInterval(this.intervalId);
+              return null;
+            }
+          });
+        }, 5000); // 5000 milliseconds = 5 seconds
+      }
+    
+      componentWillUnmount() {
+        // Clear the interval to avoid memory leaks
+        clearInterval(this.intervalId);
+      }
+    
+    render(prevProps,prevState) {
+       
         return (
             <div className="BoxAnimation">
                 {/* <div className="box"></div> */}
                 {this.state.boxNum === 0 ? <div className="box"></div> : ""}
-                {this.state.boxNum === 1 ? <div className="box2"></div> : ""}
-                {this.state.boxNum === 2 ? <div className="box3"></div> : ""}
+                {this.state.boxNum === 1 ? <div className="box"></div> : ""}
+                {this.state.boxNum === 2 ? <div className="box"></div> : ""}
             </div>
         );
     }
