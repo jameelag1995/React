8f045800d2e144ce7c566057a77c41e41dd000a3
jameel Agbaria <agbariajameel@gmail.com>
2023-12-12 09:59:35 +0200

    cancel request

--- class-components/src/App.jsx
+++ class-components/src/App.jsx
@@ -1,29 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Counter from './components/Counter'
-import Todo from './components/Todo'
-import WhatIsTime from './components/WhatIsTime'
-import Box from './components/Box'
-import CountriesOfTheWorld from './components/CountriesOfTheWorld'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Counter from "./components/Counter";
+import Todo from "./components/Todo";
+import WhatIsTime from "./components/WhatIsTime";
+import Box from "./components/Box";
+import CountriesOfTheWorld from "./components/CountriesOfTheWorld";
+import ToggleApp from "./components/CancelRequest";
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-    {/* ex 20.1 */}
-     {/* <Counter /> */}
-     {/* ex 20.2 */}
-     {/* <Todo/> */}
-     {/* ex 20.3 */}
-     {/* <WhatIsTime/> */}
-     {/* ex 21.1 */}
-     {/* <Box/> */}
-     {/* ex 21.2 */}
-     <CountriesOfTheWorld/>
-    </>
-  )
+    return (
+        <>
+            {/* ex 20.1 */}
+            {/* <Counter /> */}
+            {/* ex 20.2 */}
+            {/* <Todo/> */}
+            {/* ex 20.3 */}
+            {/* <WhatIsTime/> */}
+            {/* ex 21.1 */}
+            {/* <Box/> */}
+            {/* ex 21.2 */}
+            {/* <CountriesOfTheWorld/> */}
+            {/* ex 21.3 */}
+            <ToggleApp />
+        </>
+    );
 }
 
-export default App
+export default App;
